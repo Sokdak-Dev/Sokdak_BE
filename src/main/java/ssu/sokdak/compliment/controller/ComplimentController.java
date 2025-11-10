eaa38e4882b62ff3b5a942d10929b2dd2d222ca3
dev-cyan <zyos1@naver.com>
2025-11-10 21:31:32 +0900

    feat: 칭찬 생성 컨트롤러 메서드 추가

--- src/main/java/ssu/sokdak/compliment/controller/ComplimentController.java
+++ src/main/java/ssu/sokdak/compliment/controller/ComplimentController.java
@@ -2,11 +2,16 @@ package ssu.sokdak.compliment.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ssu.sokdak.compliment.dto.ComplimentGenerateResponse;
 import ssu.sokdak.compliment.dto.ComplimentTemplateRequest;
 import ssu.sokdak.compliment.service.ComplimentService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +24,12 @@ public class ComplimentController {
     public ResponseEntity<Void> saveComplimentTemplate(ComplimentTemplateRequest request) {
         complimentService.saveComplimentTemplate(request.getText(), request.getCategory());
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/compliments/clubs/{club_id}/users/{user_id}")
+    public ResponseEntity<List<ComplimentGenerateResponse>> createCompliments(@PathVariable("club_id") Long clubId,
+                                                                                @PathVariable("user_id") Long userId) {
+        List<ComplimentGenerateResponse> responses = complimentService.createCompliments(clubId, userId);
+        return ResponseEntity.ok(responses);
     }
 }
