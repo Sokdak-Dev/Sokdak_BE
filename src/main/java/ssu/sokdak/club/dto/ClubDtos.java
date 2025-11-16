a6ac787925f72356bb34a9abf1018f6368b5c3d8
sungyoon <tjddbs2395@naver.com>
2025-11-17 01:52:27 +0900

    [Feat] 동아리 가입신청 및 승인/거절

--- src/main/java/ssu/sokdak/club/dto/ClubDtos.java
+++ src/main/java/ssu/sokdak/club/dto/ClubDtos.java
@@ -8,6 +8,27 @@ public class ClubDtos {
     // 생성된 clubId 반환
     public record CreateClubResponse(Long clubId) { }
 
-    // 삭제된 clubId와 결과 문자열 ex) " ~ 동아리가 삭제되었습니다."
+    // 삭제된 clubId와 결과 문자열 ex) "~ 동아리가 삭제되었습니다."
     public record DeleteClubResponse(Long clubId, String result) { }
+
+    // 동아리 가입 신청 결과 응답
+    public record JoinClubResponse(
+            Long clubId,
+            Long userId,
+            String requestStatus
+    ) { }
+
+    // 동아리 가입 승인 결과 응답
+    public record ApproveClubMemberResponse(
+            Long clubId,
+            Long userId,
+            String requestStatus
+    ) { }
+
+    // 동아리 가입 거절 결과 응답
+    public record RejectClubMemberResponse(
+            Long clubId,
+            Long userId,
+            String requestStatus
+    ) { }
 }
